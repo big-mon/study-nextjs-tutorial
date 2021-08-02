2caeb1d440ff755718bcb68a4a60c79ef1bff165
BIG-MON <mrmon93@gmail.com>
2021-08-03 01:21:47 +0900

    feat: rendering markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,9 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -18,6 +19,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
